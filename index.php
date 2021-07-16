a9c7cac083d2580c4567f69298f08c5beb81cfdb
Edson Emanoel <edson@app100.com.br>
2021-07-16 10:14:40 -0300

    criei a conexao com fecth

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+    $usuario = 'root';
+    $senha = '';
+
+    try {
+        $conexao = new PDO($dsn, $usuario, $senha);
+
+        $query = '
+            select*from tb_usuarios
+        ';
+
+        $stmt = $conexao->query($query);
+        $lista = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+        echo '<pre>';
+        print_r($lista);
+        echo'</pre>';
+
+        echo $lista[1]->email;
+
+    } catch (PDOException $e) {
+        echo 'Erro:'.$e->getCode().'Mensagem: '.$e->getMessage();
+        //erro
+
+    }
