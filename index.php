d96e3fb7cd21c59c2d29efcddf8450e23ca96230
Edson Emanoel <edson@app100.com.br>
2021-07-16 11:26:07 -0300

    login com pdo

--- index.php
+++ index.php
@@ -5,31 +5,30 @@
     $senha = '';
 
     try {
-
         $conexao = new PDO($dsn, $usuario, $senha);
 
-        $query = ' select*from tb_usuarios';
-
-        // $stmt = $conexao->query($query);
-
-        foreach( $conexao->query($query) as $chave => $valor){
-            print_r($valor);
-        }
-
-        //     $lista_usuario = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        // echo '<pre>';
-        // print_r($lista_usuario);
-        // echo'</pre>';
-
-
-        //     foreach($lista_usuario as $key => $value){
-        //         echo $value['nome'];
-        //         echo '<hr>';
-        //     }
-
     }catch (PDOException $e) {
+
          echo "Erro Code : .$e->getCode() ";
          echo "Mensagem: ' .$e->getMessage()";
          echo "Arquivo: '  .$e->getFile()";
     }
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <form action="#">
+
+            <input type="text" placeholder="usuario">
+            <br><br>
+            <input type="password" placeholder="senha">
+            <br><br>
+            <button type="submit">Entrar</button>
+
+        </form>
+    </body>
+</html>
